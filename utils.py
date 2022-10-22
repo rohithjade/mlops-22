96f5a2b6b528b409120d10225ca04bdf845a69b2
Rohith <rohith.jade@gmail.com>
2022-10-23 01:24:55 +0530

    added code for model comparision

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from joblib import dump
 from sklearn import svm
 
 
-def get_all_h_param_comb(params):
+def get_all_h_param_comb_svm(params):
     h_param_comb = [{"gamma": g, "C": c} for g in params['gamma'] for c in params['C']]
     return h_param_comb
     
