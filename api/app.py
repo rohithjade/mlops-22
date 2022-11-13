517bca57025555f53d85c5644cfc9f935f12c68f
Rohith <rohith.jade@gmail.com>
2022-11-14 03:06:49 +0530

    added code for containerized api

--- api/app.py
+++ api/app.py
@@ -16,18 +16,29 @@ def hello_world():
 #     - get call / methods
 #     - post call / methods ** 
 
-@app.route("/sum", methods=['POST'])
-def sum():
-    x = request.json['x']
-    y = request.json['y']
-    z = x + y 
-    return {'sum':z}
+# @app.route("/sum", methods=['POST'])
+# def sum():
+#     x = request.json['x']
+#     y = request.json['y']
+#     z = x + y 
+#     return {'sum':z}
 
 
 
-@app.route("/predict", methods=['POST'])
-def predict_digit():
-    image = request.json['image']
-    print("done loading")
-    predicted = model.predict([image])
-    return {"y_predicted":int(predicted[0])}
+# @app.route("/predict", methods=['POST'])
+# def predict_digit():
+#     image = request.json['image']
+#     predicted = model.predict([image])
+#     return {"y_predicted":int(predicted[0])}
+
+# code for comparing the predictions of two images
+@app.route("/compare_predict", methods=['POST'])
+def compare_prediction():
+    image1 = request.json['image1']
+    image2 = request.json['image2']
+    predicted1 = model.predict([image1])
+    predicted2 = model.predict([image2])
+    if int(predicted1[0]) == int(predicted2[0]):
+        return {"y_predicted1":int(predicted1[0]) , "y_predicted2":int(predicted2[0]) , "Comparision result":"Same"}
+    else:
+        return {"y_predicted1":int(predicted1[0]) , "y_predicted2":int(predicted2[0]) , "Comparision result":"Not Same"}
